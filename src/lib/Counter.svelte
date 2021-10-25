85d2966cd39bb4420488098448e5d3fa2faca465
deepgreenAN <asami73dgreen63@gmail.com>
2021-10-25 13:26:07 +0900

    first commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let count: number = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button class="px-8 py-4 w-52 text-red-500 bg-red-200 rounded-full" on:click={increment}>
+  Clicks: {count}
+</button>
